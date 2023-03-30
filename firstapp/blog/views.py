35cce584393d0ee3ba9ae9be18533f47f63bbc1c
Bousbih <fatoumabousbih@yahoo.fr>
2023-03-30 09:57:57 +0200

    travail matin ave charlene

--- firstapp/blog/views.py
+++ firstapp/blog/views.py
@@ -2,10 +2,8 @@ import nltk
 from nltk.chat.util import Chat, reflections
 from django.shortcuts import render
 from chatbot.models import Echange
+import unicodedata
 
-nltk.download('punkt')
-nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger')
 
 
 # Create your views here.
@@ -14,7 +12,7 @@ def home(request):
     text ="<h1>Bienvenue sur mon blog !</h1>"
     text = text+ "<p>premier texte de présentation !</p>"
 
-    template= loader.get_template('index.html') 
+    template= loader.get_template('index.html')
     #strAge = request.GET['age']
     data={'prenom': 'tekfa', 
           'montres':['tissot', 'mondaine','seiko'],
@@ -73,6 +71,10 @@ def chatbot(request):
     [
         r"hi|hey|hello",
         ["Hello", "Hey there",]
+    ], 
+     [
+        r"remplacer viande",
+        ["Voici quelques-unes des options les plus courantes : Les légumineuses, les produits à base de soja, les champignons, les substituts de viande à base de plantes.",]
     ], 
     [
         r"what is your name ?",
@@ -139,9 +141,10 @@ def chatbot(request):
         ["Brad Pitt"]
     ],
     [
-        r"i am looking for online guides and courses to learn data science, can you suggest?",
-        ["Crazy_Tech has many great articles with each step explanation along with code, you can explore"]
+        r"Quels sont les différents régimes alimentaires qui existent ?",
+        ["Voici plusieurs régimes alimentaires : Régime méditerranéen, Régime végétarien, Régime végan, Régime sans gluten, Régime cétogène, Régime Paléo, Régime flexitarien. Ces régimes ne sont pas exhaustifs et il existe de nombreuses autres options, chacune ayant des avantages et des inconvénients. Il est important de consulter un professionnel de la santé avant de commencer tout régime alimentaire pour s'assurer qu'il répond à vos besoins."]
     ],
+    
     [
         r"quit",
         ["BBye take care. See you soon :) ","It was nice talking to you. See you soon :)"]
@@ -158,9 +161,12 @@ def chatbot(request):
             # Ajout de la paire à la liste des paires
             pairs.append(pair)
 
-      chat = Chat(pairs)
+      chat = Chat(pairs, reflections)
       question = request.POST.get('question')
-      response = chat.respond(question)
+      texte_normalized = unicodedata.normalize('NFKD', question).encode('ASCII', 'ignore').decode('utf-8')
+      response = chat.respond(texte_normalized)
+      
+      # Normalisation Unicode
       messages = [{'sender': 'user', 'text': question}, {'sender': 'bot', 'text': response}]
       return render(request, 'chatbot.html', {'messages': messages})
     else:
