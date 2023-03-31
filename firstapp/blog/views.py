70ea2aa07c6ed3718f281a9d1c821f4838488635
Bousbih <fatoumabousbih@yahoo.fr>
2023-03-31 12:28:44 +0200

    correction Charlene

--- firstapp/blog/views.py
+++ firstapp/blog/views.py
@@ -4,10 +4,15 @@ import nltk
 from nltk.chat.util import Chat, reflections
 from django.shortcuts import render
 from django. template import loader
+from django.views.decorators.csrf import csrf_exempt
 
 from chatbot.models import Echange
 import unicodedata
+from django.contrib.sessions.backends.db import SessionStore
+from django.shortcuts import redirect
 
+import json
+import ast
 
 
 # Create your views here.
@@ -57,34 +62,87 @@ def conseils(request) :
 def contact(request) :
       template= loader.get_template('contact.html') 
       data={'page':'contact'}
-
+      
       return(HttpResponse(template.render(data)))
 
-# contact
-
-
-
+@csrf_exempt
 def chatbot(request):
-   
+      template= loader.get_template('chatbot.html') 
+      data={'page':'chatbot'}
+      data['history']= []
+      if request.method == 'POST':
 
-    if request.method == 'POST':
-      pairs = []
-      for question_reponse in Echange.objects.all():
-            question = question_reponse.question
-            reponse = question_reponse.reponse
-            # Création de la paire de question-réponse correspondante
-            pair = [r"{}".format(question), reponse.split("|")]
-            # Ajout de la paire à la liste des paires
-            pairs.append(pair)
-
-      chat = Chat(pairs, reflections)
-      question = request.POST.get('question')
-      texte_normalized = unicodedata.normalize('NFKD', question).encode('ASCII', 'ignore').decode('utf-8')
-      response = chat.respond(texte_normalized)
+            # on récupère le champs caché sur le formulaire qui contient l'historique si on a déjà discuté avec le chatbot
+            texthistory = request.POST['history']
+            # si on a un historique dans le champs, il faut le récupérer au format json
+            ##### penser à faire un "import json" en début de programme
+            if (texthistory):
+                  json_dat = json.dumps(ast.literal_eval(texthistory))
+                  data['history'] = json.loads(json_dat)
+            pairs = []
+            for question_reponse in Echange.objects.all():
+                  question = question_reponse.question
+                  reponse = question_reponse.reponse
+                  # Création de la paire de question-réponse correspondante
+                  pair = [r"{}".format(question), reponse.split("|")]
+                  # Ajout de la paire à la liste des paires
+                  pairs.append(pair)
+            
+            chat = Chat(pairs, reflections)
+            question = request.POST.get('question')
+            # Normalisation Unicode
+            texte_normalized = unicodedata.normalize('NFKD', question).encode('ASCII', 'ignore').decode('utf-8')
       
-      # Normalisation Unicode
-      messages = [{'sender': 'user', 'text': question}, {'sender': 'bot', 'text': response}]
-      return render(request, 'chatbot.html', {'messages': messages})
-    else:
-        return render(request, 'chatbot.html')
-       
+            reponse = chat.respond(texte_normalized)
+            # on conserve les échange dans l'historique
+            msgUser = {"type" : "user", "content": question}
+            data['history'].append(msgUser)
+
+            msgBot = {"type" : "bot", "content": reponse}
+            data['history'].append(msgBot)
+
+      return(HttpResponse(template.render(data)))
+      # 
+
+# def chatbot(request):
+#     # Récupération des messages précédents depuis la variable de session
+#     messages = request.session.get('messages', [])
+
+#     if request.method == 'POST':
+#         pairs = []
+#         for question_reponse in Echange.objects.all():
+#             question = question_reponse.question
+#             reponse = question_reponse.reponse
+#             # Création de la paire de question-réponse correspondante
+#             pair = [r"{}".format(question), reponse.split("|")]
+#             # Ajout de la paire à la liste des paires
+#             pairs.append(pair)
+
+#         chat = Chat(pairs, reflections)
+#         question = request.POST.get('question')
+#         texte_normalized = unicodedata.normalize('NFKD', question).encode('ASCII', 'ignore').decode('utf-8')
+#         response = chat.respond(texte_normalized)
+
+#         # Normalisation Unicode
+#         messages.append({'sender': 'user', 'text': question})
+#         for reponse in response.split("|"):
+#             messages.append({'sender': 'bot', 'text': reponse})
+
+#         # Stockage des messages dans la variable de session
+#         request.session['messages'] = messages
+#         request.session.modified = True
+
+#     else:
+#         pairs = []
+#         for question_reponse in Echange.objects.all():
+#             question = question_reponse.question
+#             reponse = question_reponse.reponse
+#             # Création de la paire de question-réponse correspondante
+#             pair = [r"{}".format(question), reponse.split("|")]
+#             # Ajout de la paire à la liste des paires
+#             pairs.append(pair)
+
+#         chat = Chat(pairs, reflections)
+#         messages.append({'sender': 'bot', 'text': "Bonjour, que puis-je faire pour vous ?"})
+
+#     return render(request, 'chatbot.html', {'messages': messages})
--- firstapp/chatbot/views.py
+++ firstapp/chatbot/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .models import Message 
 from .chatbot import get_bot_response
+
 # Create your views here.
 from django.shortcuts import render
 import nltk
@@ -11,37 +12,29 @@ pairs = [
     # Ajoutez vos paires question / réponse ici
 ]
 
-def chatbot(request):
-    if request.method == 'POST':
-        question = request.POST.get('question')
-        oldquestion = request.POST.get('oldechange')
-        chat = Chat(pairs, reflections)
-        response = chat.respond(question)
-        oldechange = oldquestion + question + response
-        return render(request, 'chatbot.html', {'messages' : oldechange, 'question': question, 'response': response})
-    else:
-        return render(request, 'chatbot.html')
+# def chatbot(request):
+#     if request.method == 'POST':
+#         question = request.POST.get('question')
+#         oldquestion = request.POST.get('oldechange')
+#         chat = Chat(pairs, reflections)
+#         response = chat.respond(question)
+#         oldechange = oldquestion + question + response
+#         return render(request, 'chatbot.html', {'messages' : oldechange, 'question': question, 'response': response})
+#     else:
+#         return render(request, 'chatbot.html')
     
 
-
-
-
-
-
-
-
-
-@csrf_exempt
-def chatbot(request):
-    if request.method == 'POST':
-        text = request.POST.get('question')
-        oldechange = request.POST.get('oldechange')
-        if text:
-            message = Message.objects.create(sender='user', text=text)
-            response = get_bot_response(text)
-            Message.objects.create(sender='bot', text=response)
-            oldechange += f"{text}\n{response}\n"
-    else:
-        oldechange = ''
-    messages = Message.objects.all()
-    return render(request, 'chatbot.html', {'messages': messages, 'oldechange': oldechange})
+# @csrf_exempt
+# def chatbot(request):
+#     if request.method == 'POST':
+#         text = request.POST.get('question')
+#         oldechange = request.POST.get('oldechange')
+#         if text:
+#             message = Message.objects.create(sender='user', text=text)
+#             response = get_bot_response(text)
+#             Message.objects.create(sender='bot', text=response)
+#             oldechange += f"{text}\n{response}\n"
+#     else:
+#         oldechange = ''
+#     messages = Message.objects.all()
+#     return render(request, 'chatbot.html', {'messages': messages, 'oldechange': oldechange})
